bab422bd84606ac5ce7210711bb1b7514a15f746
Phil Hawksworth <phil@netlify.com>
2019-12-10 11:50:44 +0000

    NLM trasnforms in template

--- src/templates/image.js
+++ src/templates/image.js
@@ -1,2 +1,8 @@
 module.exports = (data) =>
-`<img src="${data.src}" alt="${data.alt}" />`;
+`<img srcset="${data.src}?nf_resize=fit&w=320 320w,
+             ${data.src}?nf_resize=fit&w=480 480w,
+             ${data.src}?nf_resize=fit&w=800 800w"
+     sizes="(max-width: 320px) 280px,
+            (max-width: 480px) 440px,
+            800px"
+     src="${data.src}?nf_resize=fit&w=480" alt="${data.alt}"></img>`;

d97f040164b37a9d9b132b4ed638098ca81b26e9
Phil Hawksworth <phil@netlify.com>
2019-12-10 14:34:13 +0000

    fix missing quote

--- src/templates/image.js
+++ src/templates/image.js
@@ -1,6 +1,6 @@
 module.exports = (data) =>
 `<picture class="lazy">
-  <source srcset="${data.src}?nf_resize=fit&w=20 media="(min-width: 1000px)">
+  <source srcset="${data.src}?nf_resize=fit&w=20" media="(min-width: 1000px)">
   <source srcset="${data.src}?nf_resize=fit&w=20" media="(min-width: 600px)">
   <source srcset="${data.src}?nf_resize=fit&w=20" media="(min-width: 400px)">
   <img src="${data.src}?nf_resize=fit&w=20" alt="${data.alt}" />
